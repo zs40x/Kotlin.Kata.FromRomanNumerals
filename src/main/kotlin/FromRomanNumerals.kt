e3f0e211b06faba37a10fa626907081c1dcc430d
scx <stefanmehnert@gmail.com>
2020-11-08 11:22:21 +0100

    Implemented subtractionRule to pass the IX = 9.

--- src/main/kotlin/FromRomanNumerals.kt
+++ src/main/kotlin/FromRomanNumerals.kt
@@ -2,8 +2,13 @@ import java.lang.Error
 
 class FromRomanNumerals {
 
-    fun convertToNumber(roman: String) =
-        roman.asSequence().map { romanToNumber(it) }.sum();
+    fun convertToNumber(roman: String): Int {
+        var latin =
+            roman.asSequence().map { romanToNumber(it) }
+
+        if (latin.count() > 1) return applySubtractionRule(latin).sum()
+        else return latin.sum()
+    }
 
     private fun romanToNumber(roman: Char) =
         when (roman) {
@@ -13,5 +18,10 @@ class FromRomanNumerals {
             else -> {
                 throw Error("Unexpected roman numeral character: $roman")
             }
+        }
+
+    private fun applySubtractionRule(numbers: Sequence<Int>) =
+        numbers.zipWithNext { a, b ->
+            if (a >= b) a + b else b - a
         }
 }
